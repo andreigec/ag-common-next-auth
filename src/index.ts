8850dccfb6e674c828bf8b77e2f53e55f2ee1391
Andrei Gec <andreigec@hotmail.com>
2023-09-27 14:20:58 +1000

    w

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './exported';
 export * from './getCognitoAuthOptions';
 export * from './getNextAppRequestAndSession';
+export * from './helpers/getNextAppRequest';
 export * from './helpers/getServerSession';
 export * from './SessionProvider';
 export * from './types';
